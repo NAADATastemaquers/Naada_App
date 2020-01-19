35cee734a350cf1fc6a495ad79ddea86a187e741
Balu Babu <balub997@gmail.com>
2020-01-19 14:21:06 +0530

    able to display bottom nav bar

--- app/src/main/java/com/example/naada/view/MainActivity.java
+++ app/src/main/java/com/example/naada/view/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.naada.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
 
 import com.example.naada.R;
+import com.example.naada.util.BottomNavHelper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    private void NavBarSetup() {
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottonNav);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
+        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) bottomNavigationView.getLayoutParams();
+        layoutParams.setBehavior(new BottomNavHelper());
+        BottomNavHelper.switchActivities(MainActivity.this,bottomNavigationView);
+    }
 
 }
