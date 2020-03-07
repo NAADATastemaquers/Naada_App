e26515087dd2c77f12981627174ed7cb1370b1ae
krishnamurthy1968 <kmoorthy.blr@gmail.com>
2020-03-07 13:49:20 +0530

    first commit

--- app/src/main/java/com/example/naada/view/LoginActivity.java
+++ app/src/main/java/com/example/naada/view/LoginActivity.java
@@ -10,11 +10,12 @@ import android.widget.Toast;
 
 //import com.airbnb.lottie.LottieAnimationView;
 import com.example.naada.R;
+import com.example.naada.util.BottomNavHelper;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView signUpBtn;
-
+    private TextView musicBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +36,15 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent signUppage=new Intent(LoginActivity.this,SignUp.class);
                 startActivity(signUppage);
+                finish();
+            }
+        });
+        musicBtn=findViewById(R.id.gotomusic);
+        musicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent music=new Intent(LoginActivity.this,MusicPlayerActivity.class);
+                startActivity(music);
                 finish();
             }
         });
