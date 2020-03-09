3b16b33e27393688ffe0de93ddafdce688292f5b
krishnamurthy1968 <kmoorthy.blr@gmail.com>
2020-03-09 19:20:21 +0530

    Google sign in complete

--- app/src/main/java/com/example/naada/view/LoginActivity.java
+++ app/src/main/java/com/example/naada/view/LoginActivity.java
@@ -1,24 +1,23 @@
 package com.example.naada.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-//import com.airbnb.lottie.LottieAnimationView;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.naada.R;
-import com.example.naada.util.BottomNavHelper;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+
+//import com.airbnb.lottie.LottieAnimationView;
 
 public class  LoginActivity extends AppCompatActivity {
 
@@ -33,6 +32,8 @@ public class  LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //GoogleSign in
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -77,6 +78,9 @@ public class  LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
     @Override
     protected void onStart() {
         super.onStart();
