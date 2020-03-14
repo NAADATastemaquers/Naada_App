be2dad38da0d68dd66f68cbb26a367f5ac60f2d6
sudha <smshreyamanoj@gmail.com>
2020-03-14 21:13:40 +0530

    music_player

--- app/src/main/java/com/example/naada/view/MainActivity.java
+++ app/src/main/java/com/example/naada/view/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.naada.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +12,6 @@ import com.example.naada.R;
 import com.example.naada.util.BottomNavHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-@SuppressLint("Registered")
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
