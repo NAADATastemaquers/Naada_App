be344e6641f959d67df334839766c80cfaa0eac8
sudha <smshreyamanoj@gmail.com>
2020-03-14 20:40:44 +0530

    music

--- app/src/main/java/com/example/naada/view/MainActivity.java
+++ app/src/main/java/com/example/naada/view/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.naada.R;
 import com.example.naada.util.BottomNavHelper;
@@ -13,18 +16,33 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 @SuppressLint("Registered")
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+    Button GoToPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         NavBarSetup();
+
+        GoToPlayer= findViewById(R.id.player);
+
+        GoToPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MusicPlayerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
     private void NavBarSetup() {
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottonNav);
+        bottomNavigationView = findViewById(R.id.bottonNav);
         bottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
         CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) bottomNavigationView.getLayoutParams();
         layoutParams.setBehavior(new BottomNavHelper());
-        BottomNavHelper.switchActivities(MainActivity.this, bottomNavigationView);
+        BottomNavHelper.switchActivities(MainActivity.this,bottomNavigationView);
     }
 
 }
