23e98cb37d5538426a0462f71a5fc3e1bb92f667
Bharani <bharani7829@gmail.com>
2020-04-07 18:16:43 +0530

    Login Ui changed 3rd version!

--- app/src/main/java/com/example/naada/util/NightMode.kt
+++ app/src/main/java/com/example/naada/util/NightMode.kt
@@ -12,37 +12,46 @@ import java.lang.Exception
 
 @SuppressLint("Registered")
 class NightMode : AppCompatActivity() {
-
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_night_mode)
-
         val appSettingPrefs: SharedPreferences = getSharedPreferences("AppSettingPrefs", 0)
         val sharedPrefsEdit: SharedPreferences.Editor = appSettingPrefs.edit()
         val isNightModeOn: Boolean = appSettingPrefs.getBoolean("NightMode", false)
-        if(!isNightModeOn){
+        if(isNightModeOn){
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            switch_btn.text = getString(R.string.disable)
+            try {
+                switch_btn.text = "Disable Dark Mode"
+            }catch (e:Exception){}
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            switch_btn.text = getString(R.string.enable)
-        }
+            try {
+                switch_btn.text = "Enable Dark Mode"
+            }catch (e:Exception){
 
-        switch_btn.setOnClickListener {
-            if(!isNightModeOn){
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                sharedPrefsEdit.putBoolean("NightMode", false)
-                sharedPrefsEdit.apply()
-
-                switch_btn.text = getString(R.string.enable)
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                sharedPrefsEdit.putBoolean("NightMode", true)
-                sharedPrefsEdit.apply()
-
-                switch_btn.text = getString(R.string.disable)
             }
         }
-
+        try {
+            switch_btn.setOnClickListener {
+                if (isNightModeOn) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    sharedPrefsEdit.putBoolean("NightMode", false)
+                    sharedPrefsEdit.apply()
+                    try {
+                        switch_btn.text = "Enable Dark Mode"
+                    } catch (e: Exception) {
+                    }
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    sharedPrefsEdit.putBoolean("NightMode", true)
+                    sharedPrefsEdit.apply()
+                    try {
+                        switch_btn.text = "Disable Dark Mode"
+                    } catch (e: Exception) {
+                    }
+                }
+            }
+        }catch (e:Exception){}
     }
 }
