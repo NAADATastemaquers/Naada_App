f2ff8b384e260ab5ff6af3ae67f25065c150e877
bhara <bharani7829@gmail.com>
2020-05-11 21:18:14 +0530

    fix

---
+++ app/src/main/java/com/tastemaquers/naada/view/PhoneActivity.java
@@ -0,0 +1,18 @@
+package com.tastemaquers.naada.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tastemaquers.naada.R;
+
+public class PhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone);
+
+
+    }
+}
